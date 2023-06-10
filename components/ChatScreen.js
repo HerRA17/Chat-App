fb1c99070934a37723dcd50c21d6937b371e7514
Hermann Rasch <hermann.rasch93@gmail.com>
2023-06-11 01:20:30 +0200

    Addition of gifted chat & bubble
    Styling the bubbles and the chat view.

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,26 +1,91 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, Keyboard, KeyboardAvoidingView, Platform } from 'react-native';
+import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
 // function Component
 const ChatScreen = ({ route, navigation }) => {
-    const { name, backgroundColor } = route.params;
+    const { name, backgroundColor, color } = route.params;
+    // 
+    const [messages, setMessages] = useState([]);
+    // 
+    const onSend = (newMessages) => {
+      setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+    };
   // passed along selected values from Start-Screen 
     useEffect(() => {
-        navigation.setOptions({ title: name})
+        setMessages([
+          {
+            _id: 1,
+            text: "Hello there, you are in the chat session!",
+            createdAt: new Date(),
+            user: {
+              _id: 2,
+              name: "React Native",
+              avatar: "https://placeimg.com/140/140/any"
+            },
+          },
+          {
+            _id: 2,
+            text: "This is a system message",
+            createdAt: new Date(),
+            system: true
+          }
+        ])
     }, []);
-    
+// setting the title to what was prev passed from Start screen
+    useEffect(() => {
+      navigation.setOptions({ title: name})
+    }, []);
+    // console.log(showAsyncStorageContentInDev());
+function renderInputToolbar (props) {
+  return (
+    <InputToolbar {...props} containerStyle={styles.toolbar} />
+  )
+}
+Keyboard.dismiss();
+// bubble rendering & styling
+const renderBubble = (props) => {
+  return <Bubble
+    {...props}
+    wrapperStyle={{ right: {
+      backgroundColor: "#2AAA8A",
+      
+      },
+      left: {
+        backgroundColor: "#FAC898"
+      }
+    }}
+  />
+}
  return (
    <View style={[styles.container, {backgroundColor: backgroundColor}]} >
-     <Text>Hello there, you are in the Chat Screen</Text>
-    </View>
+     <Text style={[styles.title, {color: color}]}>Hello there, you are in the Chat Screen</Text>
+     <GiftedChat 
+     messages={messages}
+     renderBubble={renderBubble}
+     onSend={messages=> onSend(messages)}
+     renderInputToolbar={renderInputToolbar}
+     user={{_id: 1}}
+     />
+      { Platform.OS === "android" ? <KeyboardAvoidingView behavior="height"/> : null}
+      { Platform.OS === "ios" ? <KeyboardAvoidingView behavior="height"/> : null}
+   
+     </View>
  );
 }
 // style sheet
 const styles = StyleSheet.create({
  container: {
    flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center'
- }
+  //  if(backgroundColor === "white")
+  width: "100%",
+  height: "100%"
+ },
+ title:{
+  margin: 8,
+  alignItems: "center",
+  fontSize: "15" 
+ },
+
 });
 
 export default ChatScreen;
