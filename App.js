4f98ead7462d27b73f5cd3770718a839b1072b79
Hermann Rasch <hermann.rasch93@gmail.com>
2023-06-13 21:17:01 +0200

    Firebase configuration & initialize
    Import of the required components from Firebase & Firestore for the messages logic implementation.

--- App.js
+++ App.js
@@ -6,6 +6,27 @@ import ChatScreen from "./components/ChatScreen";
 // import react Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import firebase & firestore
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjjYirTVXnYNk_pPtYRwyIxM5ydz8ZmmI",
+  authDomain: "chat-app-dde81.firebaseapp.com",
+  projectId: "chat-app-dde81",
+  storageBucket: "chat-app-dde81.appspot.com",
+  messagingSenderId: "386820352799",
+  appId: "1:386820352799:web:76afe21f04475d75bb1e2a",
+  measurementId: "G-4RYVRYC1EY"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// Initialize Cloud Firestore & get a ref to the service
+const db = getFirestore(app);
+
 // create the navigator
 const Stack = createNativeStackNavigator();
 const App = () => {
@@ -19,7 +40,9 @@ const App = () => {
         component={Start}/> 
         <Stack.Screen 
         name="ChatScreen"
-        component={ChatScreen}/>
+        >
+          {(props) => <ChatScreen {...props} db={db}/>}
+          </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
