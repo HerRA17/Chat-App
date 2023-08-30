8002bf897c45cdee607487383591537a7544a9e8
Hermann Rasch <hermann.rasch93@gmail.com>
2023-08-30 22:21:21 +0200

    configuration data with its corresponding keys from the .env file

---
+++ config.js
@@ -0,0 +1,9 @@
+// config file
+export const firebaseConfig = {
+apiKey: process.env.FIREBASE-API-KEY ,
+authDomain: process.env.FIREBASE-AUTH-DOMAIN ,
+projectId: process.env.FIREBASE-PROJECT-ID ,
+storageBucket: process.env.FIREBASE-STORAGE-BUCKET ,
+messagingSenderId: process.env.FIREBASE-MESSAGING-SENDER-ID ,
+appId: process.env.FIREBASE-APP-ID ,
+measurementId: process.env.FIREBASE-MEASURMENT-ID }
