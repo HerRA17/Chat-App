b206a7905b436f05c635332cfa0a5650f0e41537
Hermann Rasch <hermann.rasch93@gmail.com>
2023-09-04 13:57:28 +0200

    Solution to the bug with android fetch blob

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -11,6 +11,40 @@ import * as Location from "expo-location";
 import { uploadBytes, ref, getDownloadURL } from "firebase/storage";
 import { useCallback, useEffect, useState } from "react";
 
+
+/**
+ * Function to convert a URI to a Blob object
+ * @param {string} uri - The URI of the file
+ * @returns {Promise} - Returns a promise that resolves with the Blob object
+ */
+function uriToBlob(uri) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    // If successful -> return with blob
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
+
+    // reject on error
+    xhr.onerror = function () {
+      reject(new Error('uriToBlob failed'));
+    };
+
+    // Set the response type to 'blob' - this means the server's response 
+    // will be accessed as a binary object
+    xhr.responseType = 'blob';
+
+    // Initialize the request. The third argument set to 'true' denotes 
+    // that the request is asynchronous
+    xhr.open('GET', uri, true);
+
+    // Send the request. The 'null' argument means that no body content is given for the request
+    xhr.send(null);
+  });
+};
+
+
 const CustomActions = ({
   wrapperStyle,
   iconTextStyle,
@@ -54,8 +88,7 @@ const CustomActions = ({
   const uploadAndSendImage = async (imageURI) => {
     const uniqueRefString = generateReference(imageURI);
     const newUploadRef = ref(storage, uniqueRefString);
-    const response = await fetch(imageURI);
-    const blob = await response.blob();
+    const blob = await uriToBlob(imageURI);
     uploadBytes(newUploadRef, blob).then(async (snapshot) => {
       const imageURL = await getDownloadURL(snapshot.ref);
       onSend({ image: imageURL });
